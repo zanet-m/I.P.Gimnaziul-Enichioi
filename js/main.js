f20b2ea44051b9f66ac255676576fb2f91822af0
zanet-m <120591448+zanet-m@users.noreply.github.com>
2023-05-24 18:52:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+AOS.init({
+    duration: 800,
+    once: true
+});
+
+new SimpleLightbox({ elements:".galerie-container a " });
+new SimpleLightbox({ elements:".orar-imagini a" });
+
+emailjs.init("jCT5QfvisquLrMfdO");
+
+function mobileMenu() {
+    var x = document.getElementById("navbar");
+
+    if (x.className === "") {
+        x.className = "mobile";
+    } else {
+        x.className = "";
+    }
+}
+
+
+function send() {
+
+    if (document.getElementById("formular").checkValidity() == false) {
+        document.getElementById("formular").reportValidity();
+        return;
+    }
+
+    var date = {
+        nume: document.getElementById("nume").value,
+        email: document.getElementById("email").value,
+        mesaj: document.getElementById("mesaj").value
+    };
+
+    emailjs.send('service_374pshs', 'template_o6lrd94', date)
+        .then(function (raspuns) {
+            alert("Mesajul a fost transmis.")
+            document.getElementById("formular").reset();
+        }, function (error) {
+            alert("Eroare la transmitere. Contactati programatorul.")
+        })
+
+}
+
